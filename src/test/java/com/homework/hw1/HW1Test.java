6364df14e4a60d5422d91224d1da2bbada8bf376
Han Li <hanlibpd@gmail.com>
2021-04-13 18:10:08 -0700

    Updated demo

--- src/test/java/com/homework/hw1/HW1Test.java
+++ src/test/java/com/homework/hw1/HW1Test.java
@@ -3,17 +3,28 @@ package test.java.com.homework.hw1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+import main.java.com.homework.hw1.Demo;
+import main.java.com.homework.hw1.HW1;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 
 class HW1Test {
     @Test
     public void testHelloWorld()
     {
+        Demo d1 = new Demo(1);
+        assertEquals(1, d1.demoClassVar());
+        assertEquals(1, d1.demoInstanceVar());
 
-        // assertion - you will see one success and one fail
-        assertEquals("Hello", "Hello!");
-        assertEquals("Hello world!\n", "Hello world!");
+        Demo d2 = new Demo(23);
+        assertEquals(2, d2.demoClassVar());
+        assertEquals(23, d2.demoInstanceVar());
+
+
+
+        assertEquals(true, d1.demoIsPrimeNumber(11));
 
     }
 
